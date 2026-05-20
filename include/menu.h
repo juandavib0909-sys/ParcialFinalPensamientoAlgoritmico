4fe29e9ce656744044a4252a41f65600bb0b4c84
juandavib0909-sys <juandavib0909@gmail.com>
2026-05-20 16:48:51 -0500

    Add menu header file with menu functions and constants

---
+++ include/menu.h
@@ -0,0 +1,40 @@
+#ifndef MENU_H
+#define MENU_H
+
+const int MENU_INICIAR = 1;
+const int MENU_SALIR = 0;
+
+const char OPCION_INICIAR = '1';
+const char OPCION_SALIR = '2';
+const char OPCION_CONTINUAR = 'c';
+const char OPCION_CONTINUAR_MAYUSCULA = 'C';
+
+
+   // MENU PRINCIPAL
+
+
+void mostrarMenuPrincipal();
+
+char leerOpcionMenu();
+
+int ejecutarMenuInicial();
+
+
+  //  PANTALLAS DEL MENU
+
+
+void mostrarPantallaInstrucciones();
+
+void mostrarPantallaObjetivo();
+
+void mostrarPantallaControles();
+
+
+  //  FUNCIONES DE APOYO DEL MENU
+
+
+void esperarContinuar();
+
+void limpiarPantallaMenu();
+
+#endif
